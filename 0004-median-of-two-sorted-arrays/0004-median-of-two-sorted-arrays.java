566e18e27d5935b1f8e1de29dd3f23c6642e626e
Mukul <mukulparasharsharma@gmail.com>
2026-04-13 03:09:36 +0530

    Time: 1 ms (100%), Space: 48.7 MB (88.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,50 +1,27 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2) return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n1;
+        int left = (n1 + n2 + 1) / 2;
         int n = n1 + n2;
-        int i = 0, j = 0;
-        int ind2 = n / 2;
-        int ind1 = ind2 - 1;
-        int count = 0;
-        int ind1El = -1, ind2El = -1;
-        while (i < n1 && j < n2) {
-            if (nums1[i] < nums2[j]) {
-                if (count == ind1) ind1El = nums1[i];
-                if (count == ind2) ind2El = nums1[i];
-                count++;
-                i++;
-            } else {
-                if (count == ind1) ind1El = nums2[j];
-                if (count == ind2) ind2El = nums2[j];
-                count++;
-                j++;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if (mid1 < n1) r1 = nums1[mid1];
+            if (mid2 < n2) r2 = nums2[mid2];
+            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return Math.max(l1, l2);
+                return (double) (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
             }
-            if (ind1El != -1 && ind2El != -1) {
-                break;
-            }
+            else if (l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
         }
-        while (i < n1) {
-            if (count == ind1) ind1El = nums1[i];
-            if (count == ind2) ind2El = nums1[i];
-            count++;
-            i++;
-            if (ind1El != -1 && ind2El != -1) {
-                break;
-            }
-        }
-        while (j < n2) {
-            if (count == ind1) ind1El = nums2[j];
-            if (count == ind2) ind2El = nums2[j];
-            count++;
-            j++;
-            if (ind1El != -1 && ind2El != -1) {
-                break;
-            }
-        }
-        if (n % 2 == 1) {
-            return ind2El;
-        } else {
-            return (double) ((double) (ind1El + ind2El)) / 2.0;
-        }
+        return 0;
     }
 }
