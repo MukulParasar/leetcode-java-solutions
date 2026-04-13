d66a4c1f6198c6c125b3c31fd72b95362ea01f19
Mukul <mukulparasharsharma@gmail.com>
2026-04-13 22:55:20 +0530

    Time: 1 ms (100%), Space: 48.7 MB (88.84%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -16,8 +16,8 @@ class Solution {
             int l2 = Integer.MIN_VALUE;
             int r1 = Integer.MAX_VALUE;
             int r2 = Integer.MAX_VALUE;
-            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
-            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if (mid1 > 0) l1 = nums1[mid1 - 1];
+            if (mid2 > 0) l2 = nums2[mid2 - 1];
             if (mid1 < n1) r1 = nums1[mid1];
             if (mid2 < n2) r2 = nums2[mid2];
             if (l1 <= r2 && l2 <= r1) {
