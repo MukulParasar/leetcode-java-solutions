534302423e2fca03acf5e524c3477366fac86cbd
Mukul <mukulparasharsharma@gmail.com>
2026-06-01 01:01:18 +0530

    Time: 1 ms (90.46%), Space: 42.6 MB (99.12%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -3,16 +3,11 @@ class Solution {
         StringBuilder w1 = new StringBuilder();
         for (String w : word1) {
             w1.append(w);
-            System.out.println(w);
-            System.out.println(w1);
         }
         StringBuilder w2 = new StringBuilder();
         for (String w : word2) {
             w2.append(w);
-            System.out.println(w);
-            System.out.println(w2);
         }
-        System.out.println(w1 + " & " + w2);
         if (w1.toString().equals(w2.toString())) {
             return true;
         }
