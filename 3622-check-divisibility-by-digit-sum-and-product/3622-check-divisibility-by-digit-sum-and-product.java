de01da6bd86fd2310b310ab61ecaecb2856dee73
Mukul <mukulparasharsharma@gmail.com>
2026-08-23 04:23:20 +0530

    Time: 0 ms (100%), Space: 42.5 MB (42.31%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.java
@@ -5,14 +5,10 @@ class Solution {
         while (n > 0) {
             int lastDigit = n % 10;
             digitSum += lastDigit;
-            System.out.print(digitSum + " ");
             digitProduct *= lastDigit;
-            System.out.print(digitProduct);
             n /= 10;
-            System.out.println();
         }
         int sum = digitSum + digitProduct;
-        System.out.println(sum);
         return x % sum == 0;
     }
 }
